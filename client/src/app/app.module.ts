8edd7892548c6b125c2da74784ec5b0041c042e0
yxu <yxu@8FR14H2-PRI.sae.local>
2018-07-19 16:18:22 -0400

    add features to create, edit, delete a car

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,11 +8,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
 import { GiphyService } from './shared/giphy/giphy.service';
+import { CarEditComponent } from './car-edit/car-edit.component';
+
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/car-list', pathMatch: 'full' },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarListComponent
+    CarListComponent,
+    CarEditComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +43,9 @@ import { GiphyService } from './shared/giphy/giphy.service';
     MatCardModule,
     MatInputModule,
     MatListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
