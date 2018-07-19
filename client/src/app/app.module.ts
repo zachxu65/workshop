c2aa08cb6d6c995884a813a4cca505c077ce403a
yxu <yxu@8FR14H2-PRI.sae.local>
2018-07-19 14:13:38 -0400

    add and use angular-material package

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CarService } from './shared/car/car.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
@@ -13,7 +15,13 @@ import { CarListComponent } from './car-list/car-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [CarService],
   bootstrap: [AppComponent]
