f4ab4f1c708c76b2597ba03c3278ade2ab53874f
yxu <yxu@8FR14H2-PRI.sae.local>
2018-07-25 15:53:49 -0400

    change port number

--- server/src/main/java/com/okta/developer/demo/CarRepository.java
+++ server/src/main/java/com/okta/developer/demo/CarRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8080")
 interface CarRepository extends JpaRepository<Car, Long> {
 }
