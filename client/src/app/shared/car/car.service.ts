afb766d5c129dc69bce148c715a0be82b6c09077
yxu <yxu@8FR14H2-PRI.sae.local>
2018-07-25 14:47:15 -0400

    change port

--- client/src/app/shared/car/car.service.ts
+++ client/src/app/shared/car/car.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class CarService {
-  public API = '//localhost:8080';
+  public API = '//localhost:8181';
   public CAR_API = this.API + '/cars';
 
   constructor(private http: HttpClient) {
